f452f9333d424aed672ae8d8288532fcfaa1fc62
Pedro V Medeiros Crescencio <131831698+PdrVin@users.noreply.github.com>
2024-04-19 15:38:54 -0300

    Update Challenge v2

--- SistemaBancarioFuncoes/funcoesBank.py
+++ SistemaBancarioFuncoes/funcoesBank.py
@@ -1,41 +1,156 @@
-LIMITE_SAQUE = 3
+import textwrap
 
-def deposito(saldo: float, extrato: str) -> tuple:
-    value = float(input('Informe o valor que deseja depositar: R$ '))
+
+def menu():
+    title = " MENU "
+    menu = f"""
+    {title:=^22}
+    [D]\tDepositar
+    [S]\tSacar
+    [E]\tExtrato
+    [U]\tNovo Usuário
+    [N]\tNova Conta
+    [L]\tListar Contas
+    [Q]\tSair
+    => """
+    return input(textwrap.dedent(menu))
+
+
+# Deposito
+def depositar(saldo: float, value: float, extract: str, /) -> tuple:
     if value > 0:
         saldo += value
-        extrato += (f'\033[92m'
-                    f'Depósito: \tR$ {value:.2f}'
+        extract += (f'\033[92m'
+                    f'Depósito:\tR$ {value:.2f}'
                     f'\033[m\n')
+        print('\033[92m'
+              'Depósito realizado com sucesso!'
+              '\033[m')
+    
     else:
-        print('Operação falhou! Valor informado inválido.')
-    return saldo, extrato
+        print('\033[91m'
+              'Operação falhou! Valor informado inválido.'
+              '\033[m')
+    
+    return saldo, extract
 
 
-def saque(saldo: float, extrato: str, num_saques: int) -> tuple:
-    limite = 500
-    value = float(input("Informe o valor que deseja sacar: R$ "))
-    if value > saldo:
-        print('Operação falhou! Você não tem saldo suficiente.')
-    elif value > limite:
-        print('Operação falhou! Valor do saque maior que o limite permitido.')
-    elif num_saques >= LIMITE_SAQUE:
-        print('Operação falhou! Número de saques excedidos') 
+# Saque
+def sacar(*, saldo: float, value: float, extract: str, limit, num_saques: int, limite_saques: int) -> tuple:
+    saldo_exceeded = value > saldo
+    limit_exceeded = value > limit
+    saque_exceeded = num_saques > limite_saques
+    
+    if saldo_exceeded:
+        print('\033[91m'
+              'Operação falhou! Você não tem saldo suficiente.'
+              '\033[m')
+    
+    elif limit_exceeded:
+        print('\033[91m'
+              'Operação falhou! Valor do saque maior que o limite permitido.'
+              '\033[m')
+    
+    elif saque_exceeded:
+        print('\033[91m'
+              'Operação falhou! Número de saques excedidos.'
+              '\033[m')
+
     elif value > 0:
         saldo -= value
-        extrato += (f'\033[91m'
+        extract += (f'\033[91m'
                     f'Saque: \t\tR$ {value:.2f}'
                     f'\033[m\n')
         num_saques += 1
-    else:
-        print('Operação falhou! Valor informado é inválido.')
-    return saldo, extrato, num_saques
 
-def show_extrato(saldo: float, extrato: str):
-    title = 'EXTRATO'
-    print(f'{title:=^30}')
-    print("Sem movimentações" if not extrato else extrato)
+    else:
+        print('\033[91m'
+              'Operação falhou! Valor informado é inválido.'
+              '\033[m')
+
+    return saldo, extract
+
+
+# Extrato
+def visualizar_historico(saldo: float, /, *, extract: str):
+    title = ' EXTRATO '
+    print(f'{title:=^26}')
+    
+    print("Sem movimentações" if not extract else extract)
+    
     print(f'\033[94m'
-          f'Saldo: \t\tR$ {saldo:.2f}'
+          f'Saldo:\t\tR$ {saldo:.2f}'
           f'\033[m')
-    print(f'{"":=^30}')
+    
+    print(f'{"":=^26}')
+
+
+# Novas Funções
+# Criar Usuário
+def create_user(users: list) -> list:
+    cpf = input('Informe o CPF (somente números): ')
+    user = filter_users(cpf, users)
+    
+    valid_cpf = cpf.isdigit() and len(cpf) == 11
+    if not valid_cpf:
+        print('CPF inválido! Tente novamente')
+        return
+    
+    if user:
+        print("\nUsuário já cadastrado com este CPF!")
+        return
+    
+    user_data = {
+        'name':input('Informe o nome completo: '),
+        'birth_date': input('Informe a data de nascimento (dd-mm-aaaa): '),
+        'cpf': cpf,
+        'address': input('Informe o endereço (logradouro, num - bairro - cidade/uf): ')
+    }
+    
+    users.append(user_data)
+    
+    print('\033[92m'
+          'Usuário cadastrado com sucesso!'
+          '\033[m')
+
+
+# Filtrar Usuários
+def filter_users(cpf: int, users: list):
+    filtered_users = [user for user in users if user['cpf'] == cpf]
+    return filtered_users[0] if filtered_users else None
+
+
+# Criar Conta Corrente
+def create_current_account(agency: str, account_num: int, users: list) -> dict:
+    cpf = input('Informe o CPF do Usuário (somente números): ')
+    user = filter_users(cpf, users)
+    
+    valid_cpf = cpf.isdigit() and len(cpf) == 11
+    if not valid_cpf:
+        print('CPF inválido! Tente novamente')
+        return
+    
+    if user:
+        print('\033[92m'
+              'Conta criada com sucesso!'
+              '\033[m')
+        return {'agency': agency,
+                'account_num': account_num,
+                'user': user}
+    
+    print('\033[91m'
+          'Usuário não encontrado, Fluxo de criação de conta encerrado!.'
+          '\033[m')
+
+
+# Listar Contas
+def list_accounts(accounts: list):
+    for conta in accounts:
+        linha = f"""\
+            Agência:\t{conta['agency']}
+            C/C:\t\t{conta['account_num']}
+            Titular:\t{conta['user']['name']}
+        """
+        print('=' * 100)
+        print(textwrap.dedent(linha))
+    return
--- SistemaBancarioFuncoes/sistema-bancario.py
+++ SistemaBancarioFuncoes/sistema-bancario.py
@@ -1,38 +1,67 @@
-import funcoesBank as fc
+import funcoesBank as fb
 
-menu = """
-[d] = Depositar
-[s] = Sacar
-[e] = Extrato
-[q] = Sair
+# Constantes
+LIMITE_SAQUE = 3
+AGENCY = '0001'
 
-=> """
-
-# Variáveis
+# Variáveis Globais
 saldo = 0.0
+limite = 500
 extrato = ''
 num_saques = 0
+users = []
+accounts = []
 
 # Loop de Seleção
 while True:
-    option = input(menu)
+    # Exibição Menu
+    option = fb.menu().upper()
 
     # Opção Depósito
-    if option == 'd':
-        saldo, extrato = fc.deposito(saldo, extrato)
+    if option == 'D':
+        # Input
+        value = float(input('Informe o valor que deseja depositar: R$'))
+        # Retorno
+        saldo, extrato = fb.depositar(saldo, value, extrato)
 
     # Opção Saque
-    elif option == 's':
-        saldo, extrato, num_saques = fc.saque(saldo, extrato, num_saques)
+    elif option == 'S':
+        # Input
+        value = float(input("Informe o valor que deseja sacar: R$"))
+        # Retorno
+        saldo, extrato = fb.sacar(
+            saldo=saldo,
+            value=value,
+            extract=extrato,
+            limit=limite,
+            num_saques=num_saques,
+            limite_saques=LIMITE_SAQUE,
+        )
 
     # Opção Extrato
-    elif option == 'e':
-        fc.show_extrato(saldo, extrato)
+    elif option == 'E':
+        fb.visualizar_historico(saldo, extract=extrato)
+
+    # Opção Novo Usuário
+    elif option == 'U':
+        fb.create_user(users)
+
+    # Opção Nova Conta
+    elif option == 'N':
+        num_account = len(accounts) + 1
+        conta = fb.create_current_account(AGENCY, num_account, users)
+        
+        if conta:
+            accounts.append(conta)
+
+    # Opção Listar Contas
+    elif option == 'L':
+        fb.list_accounts(accounts)
 
     # Opção Sair
-    elif option == 'q':
+    elif option == 'Q':
         break
 
     # Opção Default
     else:
-        print('Operação inválida, pro favor selecione novamente a operação desejada.')
+        print('Operação inválida, por favor selecione novamente a operação desejada.')
