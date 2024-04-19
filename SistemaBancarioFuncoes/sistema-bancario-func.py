9e06e0fadd2123f567ebbac317d1526fb322b6f3
Pedro V Medeiros Crescencio <131831698+PdrVin@users.noreply.github.com>
2024-04-19 15:42:18 -0300

    Update main file

--- SistemaBancarioFuncoes/sistema-bancario-func.py
+++ SistemaBancarioFuncoes/sistema-bancario-func.py
@@ -1,67 +1,71 @@
 import funcoesBank as fb
 
-# Constantes
-LIMITE_SAQUE = 3
-AGENCY = '0001'
+def main():
+    # Constantes
+    LIMITE_SAQUE = 3
+    AGENCY = '0001'
 
-# Variáveis Globais
-saldo = 0.0
-limite = 500
-extrato = ''
-num_saques = 0
-users = []
-accounts = []
+    # Variáveis Globais
+    saldo = 0.0
+    limite = 500
+    extrato = ''
+    num_saques = 0
+    users = []
+    accounts = []
 
-# Loop de Seleção
-while True:
-    # Exibição Menu
-    option = fb.menu().upper()
+    # Loop de Seleção
+    while True:
+        # Exibição Menu
+        option = fb.menu().upper()
 
-    # Opção Depósito
-    if option == 'D':
-        # Input
-        value = float(input('Informe o valor que deseja depositar: R$'))
-        # Retorno
-        saldo, extrato = fb.depositar(saldo, value, extrato)
+        # Opção Depósito
+        if option == 'D':
+            # Input
+            value = float(input('Informe o valor que deseja depositar: R$'))
+            # Retorno
+            saldo, extrato = fb.depositar(saldo, value, extrato)
 
-    # Opção Saque
-    elif option == 'S':
-        # Input
-        value = float(input("Informe o valor que deseja sacar: R$"))
-        # Retorno
-        saldo, extrato = fb.sacar(
-            saldo=saldo,
-            value=value,
-            extract=extrato,
-            limit=limite,
-            num_saques=num_saques,
-            limite_saques=LIMITE_SAQUE,
-        )
+        # Opção Saque
+        elif option == 'S':
+            # Input
+            value = float(input("Informe o valor que deseja sacar: R$"))
+            # Retorno
+            saldo, extrato = fb.sacar(
+                saldo=saldo,
+                value=value,
+                extract=extrato,
+                limit=limite,
+                num_saques=num_saques,
+                limite_saques=LIMITE_SAQUE,
+            )
 
-    # Opção Extrato
-    elif option == 'E':
-        fb.visualizar_historico(saldo, extract=extrato)
+        # Opção Extrato
+        elif option == 'E':
+            fb.visualizar_historico(saldo, extract=extrato)
 
-    # Opção Novo Usuário
-    elif option == 'U':
-        fb.create_user(users)
+        # Opção Novo Usuário
+        elif option == 'U':
+            fb.create_user(users)
 
-    # Opção Nova Conta
-    elif option == 'N':
-        num_account = len(accounts) + 1
-        conta = fb.create_current_account(AGENCY, num_account, users)
-        
-        if conta:
-            accounts.append(conta)
+        # Opção Nova Conta
+        elif option == 'N':
+            num_account = len(accounts) + 1
+            conta = fb.create_current_account(AGENCY, num_account, users)
+            
+            if conta:
+                accounts.append(conta)
 
-    # Opção Listar Contas
-    elif option == 'L':
-        fb.list_accounts(accounts)
+        # Opção Listar Contas
+        elif option == 'L':
+            fb.list_accounts(accounts)
 
-    # Opção Sair
-    elif option == 'Q':
-        break
+        # Opção Sair
+        elif option == 'Q':
+            break
 
-    # Opção Default
-    else:
-        print('Operação inválida, por favor selecione novamente a operação desejada.')
+        # Opção Default
+        else:
+            print('Operação inválida, por favor selecione novamente a operação desejada.')
+
+
+main()
