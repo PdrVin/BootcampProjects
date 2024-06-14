03d8c3870bef9570a42d9445d77786bb22e23231
Pedro <ped.vin42cres@gmail.com>
2024-06-13 22:11:20 -0300

    Update #1

--- 07-Fast API Python/workout_api/atleta/schemas.py
+++ 07-Fast API Python/workout_api/atleta/schemas.py
@@ -2,11 +2,24 @@ from workout_api.base.schemas import BaseSchema
 from pydantic import Field, PositiveFloat
 from typing import Annotated
 
+
 class Atleta(BaseSchema):
     # Campos
-    nome: Annotated[str, Field(description='Nome do Atleta', examples='Joao', max_length=50)]
-    cpf: Annotated[str, Field(description='CPF do Atleta', examples='12345678901', max_length=11)]
-    idade: Annotated[int, Field(description='Idade do Atleta', examples='25')]
-    peso: Annotated[PositiveFloat, Field(description='Peso do Atleta', examples='75.5')]
-    altura: Annotated[PositiveFloat, Field(description='Altura do Atleta', examples='1.80')]
-    cpf: Annotated[str, Field(description='Sexo do Atleta', examples='M', max_length=1)]
+    nome: Annotated[
+        str, Field(description="Nome do Atleta", example="Joao", max_length=50)
+    ]
+    cpf: Annotated[
+        str, Field(description="CPF do Atleta", example="12345678901", max_length=11)
+    ]
+    idade: Annotated[
+        int, Field(description="Idade do Atleta", example="25")
+    ]
+    peso: Annotated[
+        PositiveFloat, Field(description="Peso do Atleta", example="75.5")
+    ]
+    altura: Annotated[
+        PositiveFloat, Field(description="Altura do Atleta", example="1.80")
+    ]
+    cpf: Annotated[
+        str, Field(description="Sexo do Atleta", example="M", max_length=1)
+    ]
